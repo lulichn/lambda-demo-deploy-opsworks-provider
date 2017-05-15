6cbad3758e3052bde94aed5696f74dab77ab99a5
lulichn <daisuke.develop@gmail.com>
2017-05-15 19:00:03 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+console.log('Loading function');
+const aws = require('aws-sdk');
+
+const getOpsWorks = () => {
+    aws.config.update({region: 'ap-northeast-1'});
+    return new aws.OpsWorks();
+}
+
+const getPipeline = () => {
+    aws.config.update({region: 'us-east-1'});
+    return new aws.CodePipeline();
+}
+
+
+exports.handler = (event, context, callback) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log(event["CodePipeline.job"]["data"]["inputArtifacts"]);
+    
+    const opsWorks = getOpsWorks();
+    const pipeline = getPipeline();
+    
+    const deployParams = {
+        Command: {
+            Name: "deploy",
+            Args: {}
+        },
+        CustomJson: JSON.stringify({
+            id: event["CodePipeline.job"]["id"],
+            artifacts: event["CodePipeline.job"]["data"]["inputArtifacts"]
+        }),
+        StackId: process.env.STACK_ID,
+        AppId: process.env.APP_ID,
+        LayerIds: [process.env.LAYER_ID]
+    };
+    
+    opsWorks.createDeployment(deployParams, (err, data) => {
+        if (err) {
+            console.log(err, err.stack); // an error occurred
+            const failureResult = {
+                jobId: event["CodePipeline.job"]["id"],
+                failureDetails: {
+                    message: "fixme",
+                    type: "JobFailed"
+                }
+            };
+            pipeline.putJobFailureResult(failureResult, callback);
+        } else {
+            console.log(data);           // successful response
+            const successResult = {
+                jobId: event["CodePipeline.job"]["id"]
+            };
+            pipeline.putJobSuccessResult(successResult, callback);
+        }
+    });
+};
+
+
